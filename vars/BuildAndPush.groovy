bc3b3227a61e753a980c76eb26e7b8e1fc48b4e8
Ahmed Marzouk <ahmed.marzoukk00@gmail.com>
2024-12-12 13:02:55 +0200

    messae

--- vars/BuildAndPush.groovy
+++ vars/BuildAndPush.groovy
@@ -1,46 +1,29 @@
-// Assuming this is a shared library file vars/BuildAndPush.groovy
+// vars/BuildAndPush.groovy
 def call(Map params) {
     // Extract parameters
     def githubCredentialsId = params.githubCredentialsId
     def githubRepo = params.githubRepo
     def dockerhubCredentialsId = params.dockerhubCredentialsId
     def imageName = params.imageName
-    def imageTag = params.imageTag ?: "latest" // Default to 'latest' if no tag provided
+    def imageTag = params.imageTag ?: "latest"  // Default to 'latest' if not provided
 
-    // Start the pipeline
-    pipeline {
-        agent any  // Specify the agent to run the pipeline
+    // Clone repository
+    stage('Clone Repository') {
+        withCredentials([usernamePassword(credentialsId: githubCredentialsId, usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+            git(branch: 'main', credentialsId: githubCredentialsId, url: githubRepo)
+        }
+    }
 
-        stages {
-            // Clone repository
-            stage('Clone Repository') {
-                steps {
-                    withCredentials([usernamePassword(credentialsId: githubCredentialsId, usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-                        git(
-                            branch: 'main',
-                            credentialsId: githubCredentialsId,
-                            url: githubRepo
-                        )
-                    }
-                }
-            }
+    // Build Docker Image
+    stage('Build Docker Image') {
+        sh "docker build -t ${imageName}:${imageTag} ."
+    }
 
-            // Build Docker Image
-            stage('Build Docker Image') {
-                steps {
-                    sh "docker build -t ${imageName}:${imageTag} ."
-                }
-            }
-
-            // Push Docker Image
-            stage('Push Docker Image') {
-                steps {
-                    withCredentials([usernamePassword(credentialsId: dockerhubCredentialsId, passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
-                        sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
-                        sh "docker push ${imageName}:${imageTag}"
-                    }
-                }
-            }
+    // Push Docker Image
+    stage('Push Docker Image') {
+        withCredentials([usernamePassword(credentialsId: dockerhubCredentialsId, passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+            sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
+            sh "docker push ${imageName}:${imageTag}"
         }
     }
 }
